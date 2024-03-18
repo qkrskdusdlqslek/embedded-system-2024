857e0e7587806a26a603698b111cff3c8164e8a6
qkrskdusdlqslek <158007421+qkrskdusdlqslek@users.noreply.github.com>
2024-03-18 17:28:29 +0900

    Update q_test01.c

--- day04/q_test01.c
+++ day04/q_test01.c
@@ -1,20 +1,20 @@
 // queue에 복사해서 출력
- 2 // 입구(rear)로삽입하고, 출구(front)를 통해서 삭제함
- 3 #include <stdio.h>
- 4
- 5 void main()
- 6 {
- 7   int ary[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
- 8   int queue[20];
- 9   int rear = 0, front = 0;
-10
-11   int size = sizeof(ary) / sizeof(ary[0]);
-12 // 큐에 삽입
-13   for(int i = 0; i < size; i++) {
-14     queue
-15   }
-16 // 큐에서 삭제
-17   for(int i = 0; i < size; i++) {
-18
-19   }
-20 }
+  // 입구(rear)로삽입하고, 출구(front)를 통해서 삭제함
+  #include <stdio.h>
+ 
+ void main()
+  {
+    int ary[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int queue[20];
+    int rear = 0, front = 0;
+
+   int size = sizeof(ary) / sizeof(ary[0]);
+ // 큐에 삽입
+   for(int i = 0; i < size; i++) {
+     queue[rear++] = ary[i];
+   }
+ // 큐에서 삭제
+   for(int i = 0; i < size; i++) {
+     printf("%d\n", queue[front++]);
+   }
+ }
