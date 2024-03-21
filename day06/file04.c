fd0d83926ddbf24d0df497afc1943165c9b6da68
qkrskdusdlqslek <158007421+qkrskdusdlqslek@users.noreply.github.com>
2024-03-21 09:21:21 +0900

    Update file04.c

--- day06/file04.c
+++ day06/file04.c
@@ -2,31 +2,40 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-void error_handling(char* message);
-  //park.text - 이름, 정보/ parkCopy.text - 파일 입출력을 통해서
+
+//park.text - 이름, 정보/ parkCopy.text - 파일 입출력을 통해서
  
-  void main()
-  {
+  void error_handling(const char* message);
+    ferror(message);
+    exit(1);
+
+ void main()
+ {
    int fd;
-   char msg[] = "박나연 24세\n";
+   char buf[] = "박나연 24세\n";
 
    FILE* fp;
    fp = fopen("park.text", "w");
    if(fp ==NULL)
      error_hnadling("fopen() error!");
-   printf("file descriptor: %d\n", fd);
+   //printf("file descriptor: %d\n", fd);
 
-   if(write(fd, msg, sizeof(msg))==-1);
-     error_handling("write() error!");
+   if(fwrite(fd, buf,sizeof(char), sizeof(buf))==-1);
+     error_handling("fwrite() error!");
    fclose(fd);
 
-   if((fp = open("park.text", "r")) ==NULL); {
+   if((fp = open("park.text", "r")) == -1)
+     error_handling("open() error!");
+
+ if(read(fd, buf, sizeof(buf))== -1)
+     error_handling("read() error!");
+ /*  {
      fprintf(stderr, "Error");
      exit(1);
-   }
-   fscanf(fp, "%s", msg);
-   printf("%s\n", msg);
+   }*/
+   //fscanf(fp, "%s", buf);
+   printf("%s\n", buf);
 
-   fclose(fp);
+   close(fp);
    return 0;
  }
